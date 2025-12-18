31b37aa1b105bd37266d277d314b4771f29e180d
Gokul Barman <gokulbarman.13@gmail.com>
2025-12-18 12:49:23 +0530

    fix domain name

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const BASE_URL = "https://api.animechan.io/v1";
 app.use(cors({
-  origin: "https://anime-quote-generator-pi.vercel.app/"
+  origin: "https://anime-quote-generator-pi.vercel.app"
 }));
 
 app.get("/quote", async (req, res) => {
